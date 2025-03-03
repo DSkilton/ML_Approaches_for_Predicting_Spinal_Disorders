c9fc875a8a017d62954172babac9d6016e04f6cc
DSkilton <duncanskilton.ds@gmail.com>
2025-03-03 20:35:13 +0000

    Initial commit. Project has been used for practice up until this point. Next step is to implement ML Models using an @ABC

---
+++ interface_better_name.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class get_a_better_name_for_this:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(data):
+        pass
+
+    @abstractmethod
+    def predict(data):
+        pass
+
+    @abstractmethod
+    def accuracy():
+        pass
+
+    @abstractmethod
+    def plot():
+        pass
