d380a0c461e82b151b680d9372992a27c2e905ef
DSkilton <duncanskilton.ds@gmail.com>
2025-05-12 21:04:27 +0100

    Apply fixed threshold relabelling for abnormal. Adjusted LDA 2D plotting to use single axis

---
+++ constants.py
@@ -0,0 +1,48 @@
+# Random state
+FORTY_TWO = 42
+
+# Model Types
+CLASSIFIER = 'CLASSIFIER'
+REGRESSOR = 'REGRESSOR'
+
+# Features
+FEATURE = 'Feature'
+FEATURES = 'Features'
+CLASS = 'class'
+
+# Pelvic Measurements
+PELVIC_INCIDENCE = "pelvic_incidence"
+PELVIC_TILT = "pelvic_tilt"
+PELVIC_RADIUS = "pelvic_radius"
+PELVIC_SLOPE = "pelvic_slope"
+DIRECT_TILT = "direct_tilt"
+
+# Spinal Measurements
+LUMBAR_LORDOSIS_ANGLE = "lumbar_lordosis_angle"
+SACRAL_SLOPE = "sacral_slope"
+THORACIC_SLOPE = "thoracic_slope"
+CERVICAL_TILT = "cervical_tilt"
+SACRUM_ANGLE = "sacrum_angle"
+SCOLIOSIS_SLOPE = "scoliosis_slope"
+
+# Metrics
+DEGREE_SPONDYLOLISTHESIS = "degree_spondylolisthesis"
+R2_SCORE = "R² Score"
+RMSE = "RMSE"
+MEAN = "mean"
+MEDIAN = "median"
+UNIFORM = "uniform"
+NAN_EUCLIDEAN = "nan_euclidean"
+
+# Titles
+MEAN_IMPUTATION = "Mean Imputation"
+MEDIAN_IMPUTATION = "Median Imputation"
+UNIFORM_IMPUTATION = "Uniform Imputation"
+KNN_IMPUTATION = "KNN Imputation"
+REGRESSION_IMPUTATION = "Regression-based Imputation (Iterative)"
+
+# Colours
+BLUE = 'blue'
+GREEN = 'green'
+PURPLE = 'purple'
+ORANGE = 'orange'
